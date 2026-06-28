8a15cfb98e4ca22ed882452d2607a8943ab05a8f
bbromleyndis-cmd <b.bromley.ndis@gmail.com>
2026-06-28 19:05:03 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
-const CACHE='throne-companion-v1';
-const APP=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone(); if(e.request.method==='GET')caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{}); return res;}).catch(()=>hit)));});
+const CACHE_NAME = 'throne-companion-v3-faction';
+const APP_SHELL = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+    const copy = resp.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return resp;
+  }).catch(() => cached)));
+});
